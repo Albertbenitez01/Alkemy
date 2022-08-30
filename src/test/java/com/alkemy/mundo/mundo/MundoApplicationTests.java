71341694e25815f6481846bba67199efd8923e19
Alberto Benitez <benitezalbertogabriel@gmail.com>
2022-08-29 21:01:54 -0300

    Initial commit

---
+++ src/test/java/com/alkemy/mundo/mundo/MundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.mundo.mundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
